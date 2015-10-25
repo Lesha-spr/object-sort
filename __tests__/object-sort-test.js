de250970618b7d192507ec5e8e18851ab9433d69
Lesha-spr <voodoo.spr@gmail.com>
2015-10-25 16:45:04 +0200

    OS-0000: fixed tests.

--- __tests__/object-sort-test.js
+++ __tests__/object-sort-test.js
@@ -1,6 +1,6 @@
 jest.dontMock('../index.js');
 
-var sortObject;
+var sortObject, sorted;
 
 beforeEach(function() {
     sortObject = require('../index.js');
@@ -8,10 +8,26 @@ beforeEach(function() {
 
 describe('sort-object', function() {
     it('Should return object with sorted keys from unsorted alphabetical keys', function() {
-        expect(sortObject({c: 15, a: 5, b: 10})).toEqual({a: 5, b: 10, c: 15});
+        sorted = sortObject({c: 15, a: 5, b: 10});
+
+        expect(Object.keys(sorted)[0]).toBe('a');
+        expect(Object.keys(sorted)[1]).toBe('b');
+        expect(Object.keys(sorted)[2]).toBe('c');
+    });
+
+    it('Should return object with sorted and reversed keys from unsorted alphabetical keys', function() {
+        sorted = sortObject({c: 15, a: 5, b: 10}, true);
+
+        expect(Object.keys(sorted)[0]).toBe('c');
+        expect(Object.keys(sorted)[1]).toBe('b');
+        expect(Object.keys(sorted)[2]).toBe('a');
     });
 
     it('Should return object with sorted keys from unsorted numeric keys', function() {
-        expect(sortObject({'3': 15, '1': 5, '2': 10})).toEqual({'1': 5, '2': 10, '3': 15});
+        sorted = sortObject({'3': 15, '1': 5, '2': 10});
+
+        expect(Object.keys(sorted)[0]).toBe('1');
+        expect(Object.keys(sorted)[1]).toBe('2');
+        expect(Object.keys(sorted)[2]).toBe('3');
     });
 });
